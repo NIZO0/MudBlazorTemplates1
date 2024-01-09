3f459003e9b72a2a08d8c81ce705a8ef6863a9e3
Mohamad Nizam <70943980+NIZO0@users.noreply.github.com>
2024-01-08 21:42:29 -0300

    First Version

---
+++ MudBlazorTemplates1/Data/TarefaStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace MudBlazorTemplates1.Data
+{
+    public enum TarefaStatus
+    {
+        [Description("A tarefa está em estado normal.")]
+        Normal,
+
+        [Description("A tarefa está concluida.")]
+        Concluida,
+
+        [Description("A tarefa está com prioridade baixa.")]
+        PrioridadeBaixa,
+
+        [Description("A tarefa está com prioridade media.")]
+        PrioridadeMedia,
+
+        [Description("A tarefa está com prioridade alta.")]
+        PrioridadeAlta,
+    }
+}
